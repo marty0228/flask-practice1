89d34f07dfa6c7d3b90e344e61bc7aaf95510373
marty0228 <dhlee0228@naver.com>
2026-09-17 11:44:46 +0900

    실습4 주소3개

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<h1>메인 페이지</h1>"
+@app.route("/about")
+def about():
+    return "<h1>소개 페이지</h1>"
+@app.route("/test/<text>")
+def route_sample(text):
+    return f"<h1>{text}</h1>"
