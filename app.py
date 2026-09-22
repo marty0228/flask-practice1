b134171e145d5eab80f2262469a403e8b7f2d0ff
marty0228 <dhlee0228@naver.com>
2026-09-22 11:13:13 +0900

    실습6오류수정

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/hi/<name>")
+@app.route("/hi/<name1>")
 def hi_template_render(name1):
     return render_template("hi.html", name=name1)
+
+app.run(debug=True)
