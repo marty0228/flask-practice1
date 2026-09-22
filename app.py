d771fdf24ac23a75942da23df247a36571e7455f
marty0228 <dhlee0228@naver.com>
2026-09-22 10:42:07 +0900

    실습4  주소를 세 개 만들어보기

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<h1>메인 페이지</h1>"
+
 @app.route("/about")
 def about():
     return "<h1>소개 페이지</h1>"
+
 @app.route("/test/<text>")
 def route_sample(text):
     return f"<h1>{text}</h1>"
